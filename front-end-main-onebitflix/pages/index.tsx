160786aa3eb716d4a99c7a89ba476404e17c11ae
Jonas <jonasdalethese1@gmail.com>
2023-08-27 11:06:15 -0300

    Criação do header da página homeNoAuth

--- front-end-main-onebitflix/pages/index.tsx
+++ front-end-main-onebitflix/pages/index.tsx
@@ -1,13 +1,23 @@
 import Head from "next/head";
 import styles from "@/styles/homeNoAuth.module.scss";
+import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 
 const HomeNoAuth = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta
+          name="description"
+          content="Tenha acesso aos melhores conteúdos de programação de uma forma simple e fácil"
+        />
       </Head>
-      <main></main>
+      <main>
+        <HeaderNoAuth />
+        <h1>Home</h1>
+      </main>
     </>
   );
 };
---
+++ front-end-main-onebitflix/src/components/homeNoAuth/headerNoAuth/index.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import { Button, Container } from "reactstrap";
+
+export default function HeaderNoAuth() {
+  return (
+    <>
+      <div className={styles.ctaSection}>
+        <img src="/homeNoAuth/logoCta.png" alt="logoCta" className={styles.imgCta} />
+        <p>Se cadastre para ter acesso aos cursos</p>
+        <img src="/homeNoAuth/logoCta.png" alt="logoCta" className={styles.imgCta} />
+      </div>
+      <Container className={styles.nav}>
+        <img
+          className={styles.imgLogoNav}
+          src="/logoOnebitflix.svg"
+          alt="logo Onebitflix"
+        />
+        <div>
+          <Link href="/login">
+            <Button className={styles.navBtn} outline>
+              Entrar
+            </Button>
+          </Link>
+          <Link href="/register">
+            <Button className={styles.navBtn} outline>
+              Quero fazer parte
+            </Button>
+          </Link>
+        </div>
+      </Container>
+    </>
+  );
+}
