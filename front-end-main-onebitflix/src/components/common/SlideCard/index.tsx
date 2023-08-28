e49436fffe88c37d22804ecd40e7799cd961c701
Jonas <jonasdalethese1@gmail.com>
2023-08-28 13:04:00 -0300

    criação dos slides, componentes pai e filho, conexao com o backend, inicio do visual dos slides

---
+++ front-end-main-onebitflix/src/components/common/SlideCard/index.tsx
@@ -0,0 +1,20 @@
+import { CourseType } from "@/src/services/courseServices";
+import styles from "./style.module.scss";
+
+interface props {
+  course: CourseType;
+}
+
+export default function SlideCard({ course }: props) {
+  return (
+    <div className={styles.slide}>
+      <img
+        src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`}
+        alt={course.name}
+        className={styles.slideImg}
+      />
+      <p className={styles.slideTitle}>{course.name}</p>
+      <p className={styles.slideDescription}> {course.synopsis} </p>
+    </div>
+  );
+}
---
+++ front-end-main-onebitflix/src/services/courseServices.ts
@@ -0,0 +1,30 @@
+import api from "./api";
+
+export type EpisodesType = {
+  id: number;
+  name: string;
+  synopsis: string;
+  order: number;
+  videoUrl: string;
+  secondsLong: number;
+};
+
+export type CourseType = {
+  id: number;
+  name: string;
+  synopsis: string;
+  thumbnailUrl: string;
+  episodes?: EpisodesType[];
+};
+
+const courseService = {
+  getNewestCourses: async () => {
+    const response = await api.get("/courses").catch((err) => {
+      console.log(err.response.data.message);
+      return err.response;
+    });
+    return response;
+  },
+};
+
+export default courseService;
